a804871c5be51446abad745b913b5693a47b29d1
IvanFitiskin <ifitiskin93@gmail.com>
2022-03-21 22:01:04 +0300

    ENG-21: test for subject

---
+++ services/backend_py3/tests/test_subject.py
@@ -0,0 +1,52 @@
+import requests
+import pytest
+
+from config.backend_py3 import Config
+
+SERVICE_URL = Config.SERVICE_URL
+
+TEST_CASES = [
+    {
+        "data": [
+            {
+                "id": 1,
+                "name": "airport",
+                "translation": "аэропорт"
+            },
+            {
+                "id": 2,
+                "name": "furniture",
+                "translation": "мебель"
+            },
+            {
+                "id": 3,
+                "name": "Homework autumn 3",
+                "translation": "Домашняя работа, осень №3"
+            },
+            {
+                "id": 4,
+                "name": "lesson-1",
+                "translation": "Мой урок №1"
+            },
+            {
+                "id": 5,
+                "name": "lesson-2",
+                "translation": "Мой урок №2"
+            },
+            {
+                "id": 6,
+                "name": "profession",
+                "translation": "Профессия"
+            }
+        ],
+        "message": "SUBJECT_GET"
+    }
+]
+
+
+@pytest.mark.parametrize("page, result", TEST_CASES)
+def test_get_english_word_by_page(page, result):
+    response = requests.get(f'{SERVICE_URL}/v1/subject')
+
+    assert response.status_code == 200
+    assert response.json() == result
